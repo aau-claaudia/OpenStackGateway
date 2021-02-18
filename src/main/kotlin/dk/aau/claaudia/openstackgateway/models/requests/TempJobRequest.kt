3a6afe55641011862f5f6944cc241e8be785a3c7
Frederik <frho@its.aau.dk>
2021-02-18 15:46:36 +0100

    Remove auth from local profile. Changed job request format

--- src/main/kotlin/dk/aau/claaudia/openstackgateway/models/requests/TempJobRequest.kt
+++ src/main/kotlin/dk/aau/claaudia/openstackgateway/models/requests/TempJobRequest.kt
@@ -2,40 +2,16 @@ package dk.aau.claaudia.openstackgateway.models.requests
 
 data class TempJobRequest(
     val base_image: String,
-    val machine_template: MachineTemplate,
+    val job_id: String,
+    val machine_template: String,
     val owner_project: String,
     val owner_username: String,
-    val request: String,
     val request_parameters: RequestParameters,
     val total_grant_allocation: String
 )
 
-data class MachineTemplate(
-    val availability: Availability,
-    val balance: Any,
-    val category: Category,
-    val cpu: Int,
-    val description: String,
-    val gpu: Any,
-    val hiddenInGrantApplications: Boolean,
-    val id: String,
-    val memoryInGigs: Any,
-    val pricePerUnit: Int,
-    val priority: Int,
-    val type: String
-)
-
 data class RequestParameters(
     val pubKey: PubKey
-)
-
-data class Availability(
-    val type: String
-)
-
-data class Category(
-    val id: String,
-    val provider: String
 )
 
 data class PubKey(
