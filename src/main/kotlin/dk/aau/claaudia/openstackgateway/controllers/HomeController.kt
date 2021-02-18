0d5d2d9fa9992a50db99d0026a281bd6b0584858
Frederik <frho@its.aau.dk>
2021-02-18 14:35:30 +0100

    Redirect from root, Ignore local config

---
+++ src/main/kotlin/dk/aau/claaudia/openstackgateway/controllers/HomeController.kt
@@ -0,0 +1,16 @@
+package dk.aau.claaudia.openstackgateway.controllers
+
+import io.swagger.v3.oas.annotations.Hidden
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+class HomeController() {
+
+    @Hidden
+    @GetMapping("/")
+    fun index(): RedirectView? {
+        return RedirectView("/swagger-ui.html")
+    }
+}
