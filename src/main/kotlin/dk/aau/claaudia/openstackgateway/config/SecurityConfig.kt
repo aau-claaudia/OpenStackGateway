39e28973ec3445170ea29c9eb9e5251f84033e3d
Frederik <frho@its.aau.dk>
2021-02-18 15:46:36 +0100

    Remove auth from local profile. Changed job request format

--- src/main/kotlin/dk/aau/claaudia/openstackgateway/config/SecurityConfig.kt
+++ src/main/kotlin/dk/aau/claaudia/openstackgateway/config/SecurityConfig.kt
@@ -2,6 +2,7 @@ package dk.aau.claaudia.openstackgateway.config
 
 import dk.aau.claaudia.openstackgateway.security.JWTAuthorizationFilter
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
@@ -9,6 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy
 
 
 @Configuration
+@Profile("prod", "test", "dev")
 class JWTSecurityConfig(val appProperties: AppProperties) : WebSecurityConfigurerAdapter() {
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
@@ -29,5 +31,17 @@ class JWTSecurityConfig(val appProperties: AppProperties) : WebSecurityConfigure
             //.antMatchers("/api", "/api/**").hasAnyRole() // .hasRole("api-user")
             .anyRequest().authenticated().and()
             .addFilter(JWTAuthorizationFilter(authenticationManager(), appProperties))
+    }
+}
+
+@Configuration
+@Profile("local")
+class JWTSecurityConfigLocal(val appProperties: AppProperties) : WebSecurityConfigurerAdapter() {
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.cors().and()//.disable()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().authorizeRequests().antMatchers("/**").permitAll();
     }
 }
