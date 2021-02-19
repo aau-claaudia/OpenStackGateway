57fc9505184babc1bc5e54fa5008f3b766067a32
Frederik <frho@its.aau.dk>
2021-02-19 12:51:39 +0100

    Delete endpoints, comments

--- src/main/kotlin/dk/aau/claaudia/openstackgateway/config/SecurityConfig.kt
+++ src/main/kotlin/dk/aau/claaudia/openstackgateway/config/SecurityConfig.kt
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 
-
 @Configuration
 @Profile("prod", "test", "dev")
 class JWTSecurityConfig(val appProperties: AppProperties) : WebSecurityConfigurerAdapter() {
