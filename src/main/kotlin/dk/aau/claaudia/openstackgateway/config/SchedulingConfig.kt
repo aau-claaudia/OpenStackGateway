be93dd6d11f84c41780275ec69c554c832cea236
Frederik <frho@its.aau.dk>
2021-08-24 13:15:01 +0200

    fixed tests

--- src/main/kotlin/dk/aau/claaudia/openstackgateway/config/SchedulingConfig.kt
+++ src/main/kotlin/dk/aau/claaudia/openstackgateway/config/SchedulingConfig.kt
@@ -1,5 +1,6 @@
 package dk.aau.claaudia.openstackgateway.config
 
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.task.TaskSchedulerBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,6 +13,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 class SchedulingConfig {
 
     // This is required because the default task scheduler conflicts with the one needed for websocket support
+    @Qualifier("taskscheduler")
     @Bean
     fun taskScheduler(builder: TaskSchedulerBuilder): ThreadPoolTaskScheduler? {
         return builder.build()
