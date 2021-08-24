5cf6060c8a4f4186b283b767a8959f9c6c881e32
Frederik <frho@its.aau.dk>
2021-08-24 12:53:45 +0200

    accounting

---
+++ src/main/kotlin/dk/aau/claaudia/openstackgateway/tasks/Jobs.kt
@@ -0,0 +1,25 @@
+package dk.aau.claaudia.openstackgateway.tasks
+
+import dk.aau.claaudia.openstackgateway.extensions.getLogger
+import dk.aau.claaudia.openstackgateway.services.OpenStackService
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+class Jobs(private val openStackService: OpenStackService) {
+
+    @Scheduled(cron = "0 * * * * *")
+    fun cronScheduledTask() {
+        //logger.info("Task is running")
+        openStackService.chargeAllJobs()
+    }
+
+//    @Scheduled(initialDelay = 2000, fixedDelay = 3000)
+//    fun initialDelayScheduledTask() {
+//        logger.info("Task is running")
+//    }
+
+    companion object {
+        val logger = getLogger()
+    }
+}
